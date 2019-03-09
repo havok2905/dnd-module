eebaf9daed16c27d55044085e51ce18a739eddc4
Chris McLean <havok2905@gmail.com>
2019-03-09 17:56:24 -0500

    fleshed out quick reference component

--- src/components/quick-ref/rules.js
+++ src/components/quick-ref/rules.js
@@ -46,8 +46,71 @@ const movement = [
 ];
 
 const action = [
-
-]
+    {
+        title: 'Attack',
+        description: 'Melee or ranged attack'
+    },
+    {
+        title: 'Shove',
+        description: 'Special melee attack'
+    },
+    {
+        title: 'Dash',
+        description: 'Double movement speed'
+    },
+    {
+        title: 'Dodge',
+        description: 'Increases defenses'
+    },
+    {
+        title: 'Help',
+        description: 'Grant an ally advantage'
+    },
+    {
+        title: 'Use Shield',
+        description: 'Equip or unequip a shield'
+    },
+    {
+        title: 'Search',
+        description: 'Conduct a search'
+    },
+    {
+        title: 'USe Class Feature',
+        description: 'Some features use actions'
+    },
+    {
+        title: 'Grapple',
+        description: 'Special melee attack'
+    },
+    {
+        title: 'Cast a Spell',
+        description: 'Cast time of 1 action'
+    },
+    {
+        title: 'Disengage',
+        description: 'Prevent opportunity attacks'
+    },
+    {
+        title: 'Escape',
+        description: 'Escape a grapple'
+    },
+    {
+        title: 'Use Object',
+        description: 'Interact, use special abilities'
+    },
+    {
+        title: 'Hide',
+        description: 'Make a hide'
+    },
+    {
+        title: 'Ready',
+        description: 'Choose trigger and action'
+    },
+    {
+        title: 'Improvise',
+        description: 'Any action not on this list'
+    }
+];
 
 const bonusAction = [
     {
@@ -80,11 +143,113 @@ const reaction = [
 ];
 
 const condition = [
-
+    {
+        title: 'Blinded',
+        description: "You can't see"
+    },
+    {
+        title: 'Exhaustion',
+        description: 'You are exhausted'
+    },
+    {
+        title: 'Incapacitated',
+        description: "You can't take actions or reactions"
+    },
+    {
+        title: 'Petrified',
+        description: 'You are transformed into stone'
+    },
+    {
+        title: 'Restrained',
+        description: 'You are restrained'
+    },
+    {
+        title: 'Charmed',
+        description: 'You are charmed'
+    },
+    {
+        title: 'Frightened',
+        description: 'You are frightened'
+    },
+    {
+        title: 'Invisible',
+        description: "You can't be seen"
+    },
+    {
+        title: 'Poisoned',
+        description: 'You are poisoned'
+    },
+    {
+        title: 'Stunned',
+        description: 'You are stunned'
+    },
+    {
+        title: 'Deafened',
+        description: "You can't hear"
+    },
+    {
+        title: 'Grappled',
+        description: 'You are grappled'
+    },
+    {
+        title: 'Paralyzed',
+        description: 'You are paralyzed'
+    },
+    {
+        title: 'Prone',
+        description: 'You are prone'
+    },
+    {
+        title: 'Unconscious',
+        description: 'You are unconscious'
+    }
 ];
 
 const environmentalEffects = [
-
+    {
+        title: 'Lightly obscured',
+        description: 'Disadvantage on perception'   
+    },
+    {
+        title: 'Heavily obscured',
+        description: 'Effectively blind'   
+    },
+    {
+        title: 'Bright light',
+        description: 'Normal vision'   
+    },
+    {
+        title: 'Dim Light',
+        description: 'Lightly obscured'   
+    },
+    {
+        title: 'Darkness',
+        description: 'Heavily Obscured'   
+    },
+    {
+        title: 'Blindsight',
+        description: 'Perceive without sight'   
+    },
+    {
+        title: 'Darkvision',
+        description: 'Limited vision in darkness'   
+    },
+    {
+        title: 'Truesight',
+        description: 'See in darkness'   
+    },
+    {
+        title: 'Half cover',
+        description: 'Low wall, furniture'   
+    },
+    {
+        title: 'Three quarters cover',
+        description: 'Portcullis, arrow slit'   
+    },
+    {
+        title: 'Full cover',
+        description: 'completely concealed'   
+    }
 ];
 
 export {
--- storybook/stories/quick-ref.story.js
+++ storybook/stories/quick-ref.story.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { Page } from '../../src/components/page/page';
 import { QuickRef } from '../../src/components/quick-ref/quick-ref';
 
 storiesOf('QuickRef', module)
     .add('QuickRef', () => {   
         return (
-            <QuickRef/>
+            <Page>
+                <QuickRef/>
+            </Page>
         );
     });
