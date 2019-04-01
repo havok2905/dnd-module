878410ba2a2af9f1f36f5b3eb45ce5d5799191e7
Chris McLean <havok2905@gmail.com>
2019-04-01 08:15:03 -0400

    multiple export types, creature and spell directories

--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
-import { Button } from "./components/button/button.jsx";
-import { CollapsableContainer } from "./components/containers/collapsable-container.jsx";
-import { Columns } from "./components/containers/columns.jsx";
-import { Container } from "./components/containers/container.jsx";
-import { EmphasisBlock } from "./components/emphasis-block/emphasis-block.jsx";
-import { Footer } from "./components/footer/footer.jsx";
-import { ItemBlock } from "./components/item-block/item-block.jsx";
-import { MonsterBlock } from "./components/monster-block/monster-block.jsx";
-import { Page } from "./components/page/page.jsx";
-import { PanZoom } from "./components/pan-zoom/pan-zoom.jsx";
-import { QuickRef } from "./components/quick-ref/quick-ref.jsx";
-import { RollForm } from "./components/roll-form/roll-form.jsx";
-import { RollTable } from "./components/roll-table/roll-table.jsx";
-import { ScrollToMe } from "./components/scroll-to-me/scroll-to-me.jsx";
-import { Spell } from "./components/spell/spell.jsx";
-import { TableOfContents } from "./components/table-of-contents/table-of-contents.jsx";
+import { Components } from "./components/index";
+import { Creatures } from "./creatures/index";
+import { Services } from "./services/index";
+import { Spells } from "./spells/index";
 
-export {
-    Button,
-    CollapsableContainer,
-    Columns,
-    Container,
-    EmphasisBlock,
-    Footer,
-    ItemBlock,
-    MonsterBlock,
-    Page,
-    PanZoom,
-    QuickRef,
-    RollForm,
-    RollTable,
-    ScrollToMe,
-    Spell,
-    TableOfContents
-};
+export { Components, Creatures, Services, Spells };
---
+++ src/services/index.js
@@ -0,0 +1,4 @@
+import AbilityScoreCalculator from "./ability-score-calculator";
+import DiceRoller from "./dice-roller";
+
+export { AbilityScoreCalculator, DiceRoller };
