d4a6042f76a7d917e5d7a01be4cf794e8c677cde
Chris McLean <havok2905@gmail.com>
2019-04-01 09:08:34 -0400

    added habarashi creatures

--- src/creatures/index.js
+++ src/creatures/index.js
@@ -1,1 +1,17 @@
-export {};
+import DEMON_HOUND from "./constants/demon-hound";
+import DEMON_LIZARD from "./constants/demon-lizard";
+import DEMON_OWLBEAR from "./constants/demon-owl-bear";
+import QUEEN_SAND_BOA from "./constants/queen-sand-boa";
+import SAND_BOA_MONSTROSITY from "./constants/sand-boa-monstrosity";
+import TINY_SAND_BOA from "./constants/tiny-sand-boa";
+import TITAN_CYCLOPS from "./constants/titan-cyclops";
+
+export {
+    DEMON_HOUND,
+    DEMON_LIZARD,
+    DEMON_OWLBEAR,
+    QUEEN_SAND_BOA,
+    SAND_BOA_MONSTROSITY,
+    TINY_SAND_BOA,
+    TITAN_CYCLOPS
+};
