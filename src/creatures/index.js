d9c61c2b745c67cebc1298d026aafb6e38491657
Chris McLean <havok2905@gmail.com>
2019-04-01 09:20:22 -0400

    added creature

--- src/creatures/index.js
+++ src/creatures/index.js
@@ -1,6 +1,7 @@
 import DEMON_HOUND from "./constants/demon-hound";
 import DEMON_LIZARD from "./constants/demon-lizard";
 import DEMON_OWLBEAR from "./constants/demon-owl-bear";
+import GIANT_GHOSTLY_ARCTIC_CROCODILE from "./constants/giant-ghostly-arctic-crocodile";
 import QUEEN_SAND_BOA from "./constants/queen-sand-boa";
 import SAND_BOA_MONSTROSITY from "./constants/sand-boa-monstrosity";
 import TINY_SAND_BOA from "./constants/tiny-sand-boa";
@@ -10,6 +11,7 @@ export {
     DEMON_HOUND,
     DEMON_LIZARD,
     DEMON_OWLBEAR,
+    GIANT_GHOSTLY_ARCTIC_CROCODILE,
     QUEEN_SAND_BOA,
     SAND_BOA_MONSTROSITY,
     TINY_SAND_BOA,
