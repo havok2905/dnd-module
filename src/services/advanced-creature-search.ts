5a3b0e058ce237aa02a38f2c8ee13eadfcbb3bec
Chris McLean <havok2905@gmail.com>
2019-04-21 00:52:09 -0600

    more linting

--- src/services/advanced-creature-search.ts
+++ src/services/advanced-creature-search.ts
@@ -27,60 +27,71 @@ export class AdvancedCreatureSearch {
         return creatures.filter(item => {
             let isValid = true;
 
-            if (options.name && options.name.length > 0) {
-                if (
-                    !StringCompare.doesContainRegardlessOfCase(
-                        item.name,
-                        options.name
-                    )
-                ) {
-                    isValid = false;
-                }
-            }
+            isValid = this.doesContainRegardlessOfCase(
+                item.name,
+                options.name,
+                isValid
+            );
 
-            if (options.type && options.type.length > 0) {
-                if (
-                    !StringCompare.doesContainRegardlessOfCase(
-                        item.type,
-                        options.type
-                    )
-                ) {
-                    isValid = false;
-                }
-            }
+            isValid = this.doesContainRegardlessOfCase(
+                item.type,
+                options.type,
+                isValid
+            );
 
-            if (options.minCr && !options.maxCr) {
-                if (!this.isMoreOrEqual(item.challenge, options.minCr)) {
-                    isValid = false;
-                }
-            }
+            isValid = this.isMoreOrEqual(
+                item.challenge,
+                options.minCr,
+                isValid
+            );
 
-            if (!options.minCr && options.maxCr) {
-                if (!this.isBelowOrEqual(item.challenge, options.maxCr)) {
-                    isValid = false;
-                }
-            }
+            isValid = this.isBelowOrEqual(
+                item.challenge,
+                options.maxCr,
+                isValid
+            );
 
-            if (options.minCr && options.maxCr) {
-                if (
-                    !this.isBetween(
-                        item.challenge,
-                        options.minCr,
-                        options.maxCr
-                    )
-                ) {
-                    isValid = false;
-                }
-            }
+            isValid = this.isBetween(
+                item.challenge,
+                options.minCr,
+                options.maxCr,
+                isValid
+            );
 
-            if (options.alignment && options.alignment.length > 0) {
-                if (item.alignment !== options.alignment) {
-                    isValid = false;
-                }
-            }
+            isValid = this.areAlignmentsEqual(
+                options.alignment,
+                item.alignment,
+                isValid
+            );
 
             return isValid;
         });
+    }
+
+    private static doesContainRegardlessOfCase(
+        a: string,
+        b: string,
+        isValid: boolean
+    ) {
+        if (a && a.length > 0 && b && b.length > 0) {
+            isValid = StringCompare.doesContainRegardlessOfCase(a, b);
+        }
+
+        return isValid;
+    }
+
+    private static areAlignmentsEqual(
+        a: string,
+        b: string,
+        isValid: boolean
+    ): boolean {
+        if (a && a.length > 0 && b && b.length > 0) {
+            if (a !== b) {
+                isValid = false;
+            }
+        }
+
+        return isValid;
     }
 
     private static parseCrToNum(str: string): number {
@@ -96,25 +107,50 @@ export class AdvancedCreatureSearch {
         }
     }
 
-    private static isBetween(a: string, b: string, c: string): boolean {
-        const numA = this.parseCrToNum(a);
-        const numB = this.parseCrToNum(b);
-        const numC = this.parseCrToNum(c);
+    private static isBetween(
+        a: string,
+        b: string,
+        c: string,
+        isValid: boolean
+    ): boolean {
+        if (b && c) {
+            const numA = this.parseCrToNum(a);
+            const numB = this.parseCrToNum(b);
+            const numC = this.parseCrToNum(c);
 
-        return numA >= numB && numA <= numC;
+            isValid = numA >= numB && numA <= numC;
+        }
+
+        return isValid;
     }
 
-    private static isBelowOrEqual(a: string, b: string): boolean {
-        const numA = this.parseCrToNum(a);
-        const numB = this.parseCrToNum(b);
+    private static isBelowOrEqual(
+        a: string,
+        b: string,
+        isValid: boolean
+    ): boolean {
+        if (a && b) {
+            const numA = this.parseCrToNum(a);
+            const numB = this.parseCrToNum(b);
 
-        return numA <= numB;
+            isValid = numA <= numB;
+        }
+
+        return isValid;
     }
 
-    private static isMoreOrEqual(a: string, b: string): boolean {
-        const numA = this.parseCrToNum(a);
-        const numB = this.parseCrToNum(b);
+    private static isMoreOrEqual(
+        a: string,
+        b: string,
+        isValid: boolean
+    ): boolean {
+        if (a && b) {
+            const numA = this.parseCrToNum(a);
+            const numB = this.parseCrToNum(b);
 
-        return numA >= numB;
+            isValid = numA >= numB;
+        }
+
+        return isValid;
     }
 }
